e4165db69d8a1da2c15321a5a8c44ea42f82f062
Gary Turovsky <flyhighplato@gmail.com>
2012-11-15 12:54:40 -0600

    Making the Belief class do more of the work in determining best alphas and next beliefs.

--- src/java/masg/dd/pomdp/agent/belief/Belief.java
+++ src/java/masg/dd/pomdp/agent/belief/Belief.java
@@ -1,10 +1,13 @@
 package masg.dd.pomdp.agent.belief;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import masg.dd.AlgebraicDD;
 import masg.dd.CondProbDD;
+import masg.dd.ProbDD;
 import masg.dd.alphavector.BeliefAlphaVector;
 import masg.dd.pomdp.POMDP;
 import masg.dd.variables.DDVariable;
@@ -19,6 +22,9 @@ public class Belief {
 	HashMap< HashMap<DDVariable,Integer>, HashMap<HashMap<DDVariable,Integer>, CondProbDD> > nextBeliefFns = new HashMap< HashMap<DDVariable,Integer>, HashMap<HashMap<DDVariable,Integer>, CondProbDD> >();
 	
 	HashMap< HashMap<DDVariable,Integer>, HashMap<HashMap<DDVariable,Integer>, Double> > obsProbs = new HashMap< HashMap<DDVariable,Integer>, HashMap<HashMap<DDVariable,Integer>, Double> >();
+	
+	Random random = new Random();
+	
 	
 	public Belief(POMDP p, CondProbDD fn) {
 		this.p = p;
@@ -58,13 +64,21 @@ public class Belief {
 		}
 	}
 	
+	private HashMap<BeliefAlphaVector,Double> alphaValue = new HashMap<BeliefAlphaVector,Double>();
+	
 	public BeliefAlphaVector pickBestAlpha(List<BeliefAlphaVector> alphas) {
 		double bestVal = -Double.MAX_VALUE;
 		BeliefAlphaVector bestAlpha = null;
 		
 		for(BeliefAlphaVector alpha:alphas) {
-			AlgebraicDD valFn = alpha.getValueFunction().multiply(beliefFn);
-			double tempVal = valFn.getTotalWeight();
+			double tempVal;
+			if(alphaValue.containsKey(alpha)) {
+				tempVal = alphaValue.get(alpha).doubleValue();
+			}
+			else {
+				AlgebraicDD valFn = alpha.getValueFunction().multiply(beliefFn);
+				tempVal = valFn.getTotalWeight();
+			}
 			
 			if(tempVal>=bestVal) {
 				bestVal = tempVal;
@@ -74,12 +88,56 @@ public class Belief {
 		return bestAlpha;
 	}
 	
+	public List<BeliefAlphaVector> pickUsefulAlphas(List<BeliefAlphaVector> alphas) {
+		ArrayList<BeliefAlphaVector> usefulAlphas = new ArrayList<BeliefAlphaVector>();
+		usefulAlphas.add(pickBestAlpha(alphas));
+		
+		for(HashMap<HashMap<DDVariable,Integer>, CondProbDD> actBeliefs: nextBeliefFns.values()) {
+			for(CondProbDD nextBeliefFn:actBeliefs.values()) {
+				BeliefAlphaVector bestAlpha = null;
+				double bestVal = -Double.MAX_VALUE;
+				for(BeliefAlphaVector alpha:alphas) {
+					double tempVal;
+		
+					AlgebraicDD valFn = alpha.getValueFunction().multiply(nextBeliefFn);
+					tempVal = valFn.getTotalWeight();
+		
+					if(tempVal>=bestVal) {
+						bestVal = tempVal;
+						bestAlpha = alpha;
+					}
+				}
+				if(bestAlpha!=null) {
+					usefulAlphas.add(bestAlpha);
+				}
+			}
+		}
+		
+		return usefulAlphas;
+	}
+	
 	public CondProbDD getBeliefFunction() {
 		return beliefFn;
 	}
 	
 	public CondProbDD getNextBeliefFunction(HashMap<DDVariable,Integer> actSpacePt, HashMap<DDVariable,Integer> obsSpacePt) {
 		return nextBeliefFns.get(actSpacePt).get(obsSpacePt);
+	}
+	
+	public Belief getNextBelief(HashMap<DDVariable,Integer> actSpacePt, HashMap<DDVariable,Integer> obsSpacePt) {
+		if(!nextBeliefFns.containsKey(actSpacePt)) {
+			System.out.println("Action " + actSpacePt +" not possible!");
+			for(HashMap<DDVariable,Integer> pt:nextBeliefFns.keySet()) {
+				System.out.println("  " + pt);
+			}
+		}
+		else if(!nextBeliefFns.get(actSpacePt).containsKey(obsSpacePt)) {
+			System.out.println("Observation " + obsSpacePt +" not possible!");
+			for(HashMap<DDVariable,Integer> pt:nextBeliefFns.get(actSpacePt).keySet()) {
+				System.out.println("  " + pt);
+			}
+		}
+		return new Belief(p,nextBeliefFns.get(actSpacePt).get(obsSpacePt));
 	}
 	
 	public HashMap<HashMap<DDVariable,Integer>, Double> getObservationProbabilities(HashMap<DDVariable,Integer> actSpacePt) {
@@ -92,5 +150,38 @@ public class Belief {
 	
 	public CondProbDD getObservationProbabilityFunction(HashMap<DDVariable,Integer> actSpacePt) {
 		return obsProbFns.get(actSpacePt);
+	}
+	
+	public HashMap<DDVariable,Integer> sampleNextObservation(HashMap<DDVariable,Integer> actSpacePt) {
+		return sampleSpacePoint(p.getObservations(),getObservationProbabilityFunction(actSpacePt));
+	}
+	
+	public HashMap<DDVariable,Integer> sampleCurrentState() {
+		return sampleSpacePoint(p.getStates(),beliefFn);
+	}
+	
+	public HashMap<DDVariable,Integer> sampleSpacePoint(ArrayList<DDVariable> variables, CondProbDD probFn) {
+		HashMap<DDVariable,Integer> point = new HashMap<DDVariable,Integer>();
+		
+		for(DDVariable variable:variables) {
+			ArrayList<DDVariable> sumOutVars = new ArrayList<DDVariable>(variables);
+			sumOutVars.remove(variable);
+			
+			ProbDD probTempFn = probFn.sumOut(sumOutVars).toProbabilityFn();
+			
+			double thresh = random.nextDouble();
+			double weight = 0.0f;
+			
+			for(int i=0;i<variable.getValueCount();i++){
+				HashMap<DDVariable,Integer> tempPt = new HashMap<DDVariable,Integer>();
+				tempPt.put(variable,i);
+				weight += probTempFn.getValue(tempPt);
+				if(weight>thresh) {
+					point.put(variable,i);
+					break;
+				}
+			}
+		}
+		return point;
 	}
 }
